acd823eefe3cde09a5cac573c23b0638fe7a2c47
Sujeeth Godavarthi <satya@Satyanarayanas-MacBook-Pro.local>
2026-05-12 12:03:32 +0530

    commit 2

--- B.java
+++ B.java
@@ -1,5 +1,5 @@
 public class B {
     public static void main(String[] args){
-        System.out.println("Test 6 it is not working");
+        System.out.println("Test 6 it is not working")
     }
 }
